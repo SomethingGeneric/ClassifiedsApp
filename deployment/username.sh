535bc0279a795fe305f3cde9d748272b75378798
Matt C <mdc028@bucknell.edu>
2021-09-17 19:02:01 -0400

    whooo lord sed

--- deployment/username.sh
+++ deployment/username.sh
@@ -2,7 +2,7 @@
 
 pushd deployment
 cp classified.service classified.service.new
-sed -i "s/apps/${USER}/g" classified.service.new
+sed -i "s/apps/$USER/g" classified.service.new
 cp nginx.site nginx.site.new
-sed -i "s/apps/${USER}/g" nginx.site.new
+sed -i "s/apps/$USER/g" nginx.site.new
 popd
